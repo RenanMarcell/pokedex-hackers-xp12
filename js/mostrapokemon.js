24c534262394eb3ab9a42d359a9ce0bca82bb6d1
renan <renanm@outlook.com>
2018-03-31 13:39:00 -0300

    first and last commit

--- js/mostrapokemon.js
+++ js/mostrapokemon.js
@@ -6,8 +6,15 @@ function mostraPokemon(url) {
   //     OS COMPONENTES HTML NO MODAL
   //  4. ABRIR MODAL (.modal) COM JAVASCRIPT
   //     (http://getbootstrap.com/docs/4.0/components/modal/#via-javascript)
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.send();
+    xhr.onload = function(e) {
+        const res = JSON.parse(xhr.response);
+        populaModal(res);
+        $('.modal').modal();
+    };
 }
-
 
 // Mostrar o nome, imagem do pokemon, tipo (todos os tipos), peso, altura e id
 function populaModal(pokemon) {
@@ -17,4 +24,18 @@ function populaModal(pokemon) {
   //     (http://getbootstrap.com/docs/4.0/components/modal/#modal-components)
   //  2. LINKAR TODOS OS COMPONENTES COM O MODAL .modal
   //  3. SEMPRE QUE FECHAR O MODAL LIMPAR O CONTEUDO ADICIONADO
+    const pokemon_name = document.createElement('h5');
+    pokemon_name.innerHTML = 'Nome: ' + pokemon.name;
+    const pokemon_peso = document.createElement('h6');
+    pokemon_peso.innerHTML = 'Peso: ' + pokemon.weight;
+    const pokemon_altura = document.createElement('h6');
+    pokemon_altura.innerHTML = 'Altura: ' + pokemon.height;
+    const pokemon_imagem = document.createElement('img');
+    pokemon_imagem.classList.add('img-fluid');
+    pokemon_imagem.src = pokemon.sprites.front_default;
+    document.querySelector('.modal-body').innerHTML = '';
+    document.querySelector('.modal-body').appendChild(pokemon_imagem);
+    document.querySelector('.modal-body').appendChild(pokemon_name);
+    document.querySelector('.modal-body').appendChild(pokemon_peso);
+    document.querySelector('.modal-body').appendChild(pokemon_altura);
 }
